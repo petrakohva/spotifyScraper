843bf17274ca57704682b402114f7f0d7574b08c
Ubuntu <aleksi@ubuntualeksi.0qeaiqxt230epgimkwbdcust2b.fx.internal.cloudapp.net>
2019-12-07 11:27:49 +0000

    Removed BeautifulSoup, because it wasn't necessary

--- spotifyscraper1.py
+++ spotifyscraper1.py
@@ -1,11 +1,23 @@
 import requests
 import urllib.request
 import time
-from bs4 import BeautifulSoup
 import re
+from datetime import datetime
+
+startTime = datetime.now()
+
+# Listat
+# lista johon kerätään linkkien sisältö
+aTagList = []
+
+# Lista johon kerätään linkkilistasta albumiin viittavat rivit
+albumTagList = []
+
+# Lista johon kerätään vain albumin nimitieto
+albumNameList = []
+
 
 # Linkitys artistien Spotify -sivuille
-
 # In Flames
 url = 'https://open.spotify.com/artist/57ylwQTnFnIhJh4nu4rxCs'
 
@@ -15,35 +27,25 @@ url = 'https://open.spotify.com/artist/57ylwQTnFnIhJh4nu4rxCs'
 # Atlas
 # url = 'https://open.spotify.com/artist/33BnCqtsMZSw7LlPBwzmmH'
 
+# haetaan urlin mukaista verkkosivua ja kirjoitetaan sivun data muuttujaan
 response = requests.get(url)
-albumTagList = []
-albums = []
-print(response)
+data = response.text
 
-soup = BeautifulSoup(response.text, "html.parser")
-atags = soup.findAll('a')
-
-# Muunnetaan bs4 elementti stringiksi jatkokäsittelyä varten
-atags_str = str(atags)
-
-# Yhdistetään elementit
-tempList = "".join(atags_str)
-
-# Otetaan talteen linkkien sisältö, joista karsitaan pois kaikki paitsi linkit joiden href on album, 
-# jolloin saadaan vain tarvittava tieto
-tempList = re.findall(r"<a.*?>", tempList)
-for tag in tempList:
+# Otetaan talteen linkkien sisältö aTagListaan, joista karsitaan pois kaikki paitsi linkit joiden href on album, 
+# jolloin saadaan vain tarvittava tieto albumTagListaan
+aTagList = re.findall(r"<a.*?>", data)
+for tag in aTagList:
     if re.search(r"\/album", tag):
         albumTagList.append(tag)
 
 # Halutaan talteen vain albumien nimet, albumin nimi esiintyy alt parametrissä
-# heataan sanaa joka esiintyy merkkien: alt=" jälkeen ja loppuu: " merkkiin
+# heataan sanaa joka esiintyy merkkien: alt=" jälkeen ja loppuu ensimmäiseen: " merkkiin
 # Lopuksi karsitaan viimeinen " merkki ja tallennetaan albumin nimi albums listaan
 for line in albumTagList:
     name = re.search(r'(?<=alt=")(.*?")', line, re.MULTILINE)
-    albums.append(name.group(1)[:-1])
+    albumNameList.append(name.group(1)[:-1])
 
-    
-for album in albums:
+# Tulostetaan albumien nimet
+for album in albumNameList:
     print(album)
-
+print(datetime.now() - startTime)
